507d7783719d1f031620af491ad077066c141c96
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 15:13:41 -0700

    Fixed the map

--- coverage.php
+++ coverage.php
@@ -10,15 +10,15 @@
     <link rel="stylesheet" href="assets/css/coverage.css" />
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     <script>
-function initialize() {
-  var chicago = new google.maps.LatLng(-122.2930339, 47.5832951);
-  var mapOptions = {
-    zoom: 11,
-    center: chicago,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
+    function initialize() {
+        var seattle = new google.maps.LatLng(-122.2930339, 47.5832951);
+        var mapOptions = {
+            zoom: 11,
+            center: seattle,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 <?
     foreach(scandir("maps/") as $file) {
@@ -26,16 +26,16 @@ function initialize() {
             $jsvar = str_replace(".", "", $file);
             $jsvar = str_replace("-", "", $jsvar);
             $jsvar = "layer".$jsvar;
-            echo "    var $jsvar = new google.maps.KmlLayer({\n";
-            echo "    url: 'http://www.seattlemesh.net/maps/$file'\n";
-            echo "  });\n";
-            echo"    $jsvar.setMap(map);\n\n\n";
+            echo "        var $jsvar = new google.maps.KmlLayer({\n";
+            echo "            url: 'http://www.seattlemesh.net/maps/$file'\n";
+            echo "        });\n";
+            echo "        $jsvar.setMap(map);\n\n\n";
         }
     }
     ?>
-}
+    }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
   </head>
