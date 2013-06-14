93c7d95fcc529283e393bbb5aed921bd3ff0b4c3
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 16:03:33 -0700

    Added coverage map to the menu

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
     <a class="brand" href="/">Seattle Meshnet</a>
     <ul class="nav">
       <li><a href="/">Home</a></li>
-      <li><a href="/map">Map</a></li>
+      <li><a href="/map">Node Map</a></li>
+      <li><a href="/coverage">Coverage Map</a></li>
       <li><a href="https://chat.projectmeshnet.org/#seattlemeshnet">Chat</a></li>
       <li><a href="/cjdns">Software</a></li>
       <li><a href="http://pics.meshwith.me/">Nodes</a></li>
