7861e3a8d2ee2d6175f23ceee782b83bd198d94d
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 00:49:18 -0700

    Added some buttons and a navbar

---
+++ header.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+  <div class="navbar-inner">
+    <a class="brand" href="/">Seattle Meshnet</a>
+    <ul class="nav">
+      <li class="active"><a href="/">Home</a></li>
+      <li><a href="/map">Map</a></li>
+      <li><a href="/chat">Chat</a></li>
+      <li><a href="/cjdns">Software</a></li>
+      <li><a href="/nodes">Nodes</a></li>
+    </ul>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -4,17 +4,22 @@
         <title>Seattle Meshnet Project</title>
         <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
         <link rel="stylesheet" href="assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
     </head>
     <body>
+        <? require 'header.php'; ?>
         <div class="hero-unit">
             <h1>Seattle Meshnet</h1>
             <p>Building a decentralized, distributed mesh network in Seattle, Washington</p>
             <p>
-            <a class="btn btn-primary btn-large">
-                Learn More
+            <a class="btn btn-primary btn-large" href="/about">
+                <i class="icon-info-sign"></i> Learn More
             </a>
-            <a class="btn btn-success btn-large">
-                Get on the map
+            <a class="btn btn-info btn-large" href="https://fund.meshwith.me/">
+                <i class="icon-btc"></i><i class="icon-usd"></i> Donate
+            </a>
+            <a class="btn btn-success btn-large" href="/map">
+                <i class="icon-map-marker"></i> Get on the map
             </a>
             </p>
         </div>
