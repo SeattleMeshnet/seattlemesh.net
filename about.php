10955a84def86f91472f0a2c5840c39a6e36542b
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 01:52:30 -0700

    Added an about page

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Seattle Meshnet Project</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+    </head>
+    <body>
+        <? require 'header.php'; ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis porta eros. Vivamus tincidunt adipiscing lectus, a bibendum dui vulputate sit amet. Vivamus dignissim cursus dui at sollicitudin. Sed magna augue, euismod ac sodales nec, faucibus vitae dui. Curabitur vel venenatis mi, ut mollis elit. Integer dui tellus, pharetra commodo odio et, auctor interdum mi. Nam pharetra accumsan accumsan. In auctor dignissim mauris in volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec quis quam metus. Sed gravida iaculis dui, ac sodales nunc molestie sed. Etiam quis semper dolor, ac pellentesque odio. Etiam fringilla congue euismod. Proin iaculis faucibus dignissim. Ut nec facilisis purus.
+
+Donec odio urna, semper ut dictum id, malesuada sit amet elit. Sed sodales iaculis nisi, in consequat magna consequat sed. Curabitur a libero felis. Aliquam vulputate, risus et tincidunt pretium, magna neque congue enim, in malesuada nulla enim in ipsum. Curabitur id est scelerisque, dignissim libero vitae, feugiat orci. Etiam congue fermentum hendrerit. Suspendisse congue felis urna, ac porta dui molestie et. Maecenas dapibus eleifend elit eu tristique. Maecenas nec facilisis massa, vel sollicitudin sapien. Sed ut neque pharetra, auctor felis quis, auctor lectus. Sed cursus nulla nec diam congue sagittis et mollis eros.
+
+Nunc consequat ipsum et vestibulum blandit. Donec blandit bibendum neque nec ultrices. Pellentesque sagittis tincidunt sagittis. Donec quis lobortis est, quis viverra sem. Integer scelerisque posuere turpis, eu euismod mauris tempor at. Quisque adipiscing nec velit vel pharetra. In dignissim, tortor eget dignissim commodo, justo lacus bibendum erat, a eleifend sem tortor quis orci. Pellentesque pharetra vehicula est mattis dictum. Sed ligula eros, semper eget neque in, tempus porta mi. Fusce posuere bibendum augue non auctor. Suspendisse dapibus, eros id lobortis vulputate, sapien mi pellentesque neque, lacinia sagittis augue lacus non elit. Proin varius sapien eu mi feugiat tempor.
+
+Aliquam luctus neque non arcu dictum molestie. Vivamus vitae lorem massa. Aliquam ac ligula non dolor suscipit pulvinar eu tincidunt nisi. Nam cursus sit amet quam eu facilisis. Nam vel elementum diam. Etiam sit amet elementum dolor. Maecenas placerat in libero quis placerat.
+
+Aenean volutpat nibh at turpis posuere aliquet. In bibendum vel nulla vel porttitor. Praesent consequat enim at ipsum interdum, vitae faucibus ipsum porta. Cras sed elit fringilla, accumsan nunc ut, varius neque. Mauris tortor ligula, ultricies at volutpat laoreet, laoreet a odio. Duis elementum consectetur nisl, id pellentesque diam. In hac habitasse platea dictumst. Aenean posuere venenatis dolor, ac bibendum turpis lobortis id. Cras sed lorem magna. Fusce aliquet nibh tincidunt odio vehicula egestas. Sed vestibulum dignissim rutrum. Nunc ut dui eget mauris semper mollis. Donec tempus tempor est. Nam ac blandit est. Vestibulum id gravida mi.</p>
+        <script src="assets/js/jquery-2.0.2.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <? require 'analytics.php'; ?>
+    </body>
+</html>
