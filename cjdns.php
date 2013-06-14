1f94b083b2e5bacf071e6c9bc7b29129c70aa0b5
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 14:59:09 -0700

    Added coverage map and a short blurb about CJDNS

---
+++ cjdns.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Seattle Meshnet Project</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+    </head>
+    <body>
+        <? require 'header.php'; ?>
+        <div class="container-fluid">
+            <div class="offset1 span10">
+                <p class="lead">At Seattle Meshnet, we use
+                <a href="https://github.com/cjdelisle/cjdns">CJDNS</a> for secure,
+                decentralized mesh networking</p>
+                <p>
+                    CJDNS allows us to connect together securely by encrypting
+                    every connection from end to end, while at the same time
+                    being completely decentralized 
+                </p>
+            </div>
+        </div>
+        <script src="assets/js/jquery-2.0.2.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <? require 'analytics.php'; ?>
+    </body>
+</html>
