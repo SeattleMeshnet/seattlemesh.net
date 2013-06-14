945db123ca21f1110dc12d4ed066c1a226a9e1cc
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 01:04:46 -0700

    Linked the chat menu item to chat.projectmeshnet.org

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <ul class="nav">
       <li class="active"><a href="/">Home</a></li>
       <li><a href="/map">Map</a></li>
-      <li><a href="/chat">Chat</a></li>
+      <li><a href="https://chat.projectmeshnet.org/#seattlemeshnet">Chat</a></li>
       <li><a href="/cjdns">Software</a></li>
       <li><a href="/nodes">Nodes</a></li>
     </ul>
