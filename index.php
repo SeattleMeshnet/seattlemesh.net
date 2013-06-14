9c2a6e269c147827fa8de709d87a6cb2e014aa25
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 00:13:05 -0700

    Added bootstrap files, linked them from the index page

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-the seattle meshnet project welcomes you to our lack of site
+<html>
+    <head>
+        <title>Seattle Meshnet Project</title>
+        <link rel="stylesheet" src="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" src="assets/css/bootstrap-responsive.min.css" />
+    </head>
+    <body>
+        <script src="assets/js/jquery-2.0.2.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
