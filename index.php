eee26d2b80a90930c583d7de8220a7798b54894f
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 01:08:12 -0700

    Added analytics

--- index.php
+++ index.php
@@ -25,5 +25,6 @@
         </div>
         <script src="assets/js/jquery-2.0.2.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
+        <? require 'analytics.php'; ?>
     </body>
 </html>
