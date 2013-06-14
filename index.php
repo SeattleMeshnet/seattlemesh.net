ff368b033898dcb395e01aefdf7e78f868544f35
thefinn93 <thefinn93@thefinn93.com>
2013-06-14 00:31:58 -0700

    Updated bootstrap, fixed links

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Seattle Meshnet Project</title>
-        <link rel="stylesheet" src="assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" src="assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-responsive.min.css" />
     </head>
     <body>
+        <div class="hero-unit">
+            <h1>Seattle Meshnet</h1>
+            <p>Building a decentralized, distributed mesh network in Seattle, Washington</p>
+            <p>
+            <a class="btn btn-primary btn-large">
+                Learn More
+            </a>
+            <a class="btn btn-success btn-large">
+                Get on the map
+            </a>
+            </p>
+        </div>
         <script src="assets/js/jquery-2.0.2.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
     </body>
