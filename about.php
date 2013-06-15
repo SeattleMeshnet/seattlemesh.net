3003c377367c10c120f5551c038993a0843613bf
danry25 <adn5412@gmail.com>
2013-06-15 03:23:40 -0600

    Updated about.php, removed lorem ipsum

--- about.php
+++ about.php
@@ -10,22 +10,11 @@
         <? require 'header.php'; ?>
         <div class="container-fluid">
             <div class="offset1 span10">
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis porta eros.</p>
+                <p class="lead">What is the Seattle Meshnet Project?</p>
                 <p>
-                    Vivamus tincidunt adipiscing lectus, a bibendum dui vulputate sit amet. Vivamus dignissim cursus dui at sollicitudin. Sed magna augue, euismod ac sodales nec, faucibus vitae dui. Curabitur vel venenatis mi, ut mollis elit. Integer dui tellus, pharetra commodo odio et, auctor interdum mi. Nam pharetra accumsan accumsan. In auctor dignissim mauris in volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec quis quam metus. Sed gravida iaculis dui, ac sodales nunc molestie sed. Etiam quis semper dolor, ac pellentesque odio. Etiam fringilla congue euismod. Proin iaculis faucibus dignissim. Ut nec facilisis purus.
+                    The Seattle Meshnet Project is an attempt to build the physical infrastructure to run cjdns, a decentralized, encrypted, low latency routing protocol. Building off of previous wireless projects, we intend to create a separate network that will be resistant towards attempts to censor or otherwise impede free & legal speech, while also being resistant to natural disasters and other events that might take down ISPs today. 
                 </p>
-                <p>
-                    Donec odio urna, semper ut dictum id, malesuada sit amet elit. Sed sodales iaculis nisi, in consequat magna consequat sed. Curabitur a libero felis. Aliquam vulputate, risus et tincidunt pretium, magna neque congue enim, in malesuada nulla enim in ipsum. Curabitur id est scelerisque, dignissim libero vitae, feugiat orci. Etiam congue fermentum hendrerit. Suspendisse congue felis urna, ac porta dui molestie et. Maecenas dapibus eleifend elit eu tristique. Maecenas nec facilisis massa, vel sollicitudin sapien. Sed ut neque pharetra, auctor felis quis, auctor lectus. Sed cursus nulla nec diam congue sagittis et mollis eros.
-                </p>
-                <p>
-                    Nunc consequat ipsum et vestibulum blandit. Donec blandit bibendum neque nec ultrices. Pellentesque sagittis tincidunt sagittis. Donec quis lobortis est, quis viverra sem. Integer scelerisque posuere turpis, eu euismod mauris tempor at. Quisque adipiscing nec velit vel pharetra. In dignissim, tortor eget dignissim commodo, justo lacus bibendum erat, a eleifend sem tortor quis orci. Pellentesque pharetra vehicula est mattis dictum. Sed ligula eros, semper eget neque in, tempus porta mi. Fusce posuere bibendum augue non auctor. Suspendisse dapibus, eros id lobortis vulputate, sapien mi pellentesque neque, lacinia sagittis augue lacus non elit. Proin varius sapien eu mi feugiat tempor.
-                </p>
-                <p>
-                    Aliquam luctus neque non arcu dictum molestie. Vivamus vitae lorem massa. Aliquam ac ligula non dolor suscipit pulvinar eu tincidunt nisi. Nam cursus sit amet quam eu facilisis. Nam vel elementum diam. Etiam sit amet elementum dolor. Maecenas placerat in libero quis placerat.
-                </p>
-                <p>
-                    Aenean volutpat nibh at turpis posuere aliquet. In bibendum vel nulla vel porttitor. Praesent consequat enim at ipsum interdum, vitae faucibus ipsum porta. Cras sed elit fringilla, accumsan nunc ut, varius neque. Mauris tortor ligula, ultricies at volutpat laoreet, laoreet a odio. Duis elementum consectetur nisl, id pellentesque diam. In hac habitasse platea dictumst. Aenean posuere venenatis dolor, ac bibendum turpis lobortis id. Cras sed lorem magna. Fusce aliquet nibh tincidunt odio vehicula egestas. Sed vestibulum dignissim rutrum. Nunc ut dui eget mauris semper mollis. Donec tempus tempor est. Nam ac blandit est. Vestibulum id gravida mi.
-                </p>
+
             </div>
         </div>
         <? require 'footer.php'; ?>
