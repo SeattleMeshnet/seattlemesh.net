9afa25ca271d80284589d468765a400c88e2578f
danry25 <adn5412@gmail.com>
2013-06-15 22:46:07 -0600

    Updated cjdns.php, more work needed on this.

--- cjdns.php
+++ cjdns.php
@@ -10,13 +10,14 @@
         <? require 'header.php'; ?>
         <div class="container-fluid">
             <div class="offset1 span10">
-                <p class="lead">At Seattle Meshnet, we use
+                <p class="lead">THe Seattle Meshnet primarily uses
                 <a href="https://github.com/cjdelisle/cjdns">CJDNS</a> for secure,
                 decentralized mesh networking</p>
                 <p>
-                    CJDNS allows us to connect together securely by encrypting
+                    CJDNS is a networking suite for meshnets that allows us to connect together securely by encrypting
                     every connection from end to end, while at the same time
-                    being completely decentralized 
+                    being completely decentralized . CJDNS operates at layer 2 locally, and it can peer with other distant meshnets via a UDP interface on layer 3.
+                    
                 </p>
             </div>
         </div>
