84fd8d48f400397f7e15887de203b8c656bbc83a
thefinn93 <thefinn93@thefinn93.com>
2013-06-16 12:36:48 -0700

    Added a 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Seattle Meshnet Project</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+    </head>
+    <body>
+        <? require 'header.php'; ?>
+        <div class="container-fluid">
+            <div class="offset1 span10">
+                <p class="lead">Nope</p>
+                <p>
+                    That page doesn't exist, sorry :(
+                </p>
+
+            </div>
+        </div>
+        <? require 'footer.php'; ?>
+    </body>
+</html>
--- coverage.php
+++ coverage.php
@@ -10,15 +10,16 @@
     <link rel="stylesheet" href="assets/css/coverage.css" />
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     <script>
-    function initialize() {
-        var seattle = new google.maps.LatLng(-122.2930339, 47.5832951);
-        var mapOptions = {
-            zoom: 11,
-            center: seattle,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
+        var map;
+        function initialize() {
+            var seattle = new google.maps.LatLng(-122.2930339, 47.5832951);
+            var mapOptions = {
+                zoom: 11,
+                center: seattle,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
 
-        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 <?
     foreach(scandir("maps/") as $file) {
@@ -26,16 +27,16 @@
             $jsvar = str_replace(".", "", $file);
             $jsvar = str_replace("-", "", $jsvar);
             $jsvar = "layer".$jsvar;
-            echo "        var $jsvar = new google.maps.KmlLayer({\n";
-            echo "            url: 'https://www.seattlemesh.net/maps/$file'\n";
-            echo "        });\n";
-            echo "        $jsvar.setMap(map);\n\n\n";
+            echo "            var $jsvar = new google.maps.KmlLayer({\n";
+            echo "                url: 'https://www.seattlemesh.net/maps/$file'\n";
+            echo "            });\n";
+            echo "            $jsvar.setMap(map);\n\n\n";
         }
     }
     ?>
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+        google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
   </head>
