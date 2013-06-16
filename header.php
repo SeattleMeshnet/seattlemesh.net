e0a11c63abd19e696faaf26d610a5515e75b2452
danry25 <adn5412@gmail.com>
2013-06-15 22:33:52 -0600

    Added an Anout Us tab

--- header.php
+++ header.php
@@ -5,9 +5,10 @@
       <li><a href="/">Home</a></li>
       <li><a href="/map">Node Map</a></li>
       <li><a href="/coverage">Coverage Map</a></li>
+      <li><a href="/about">About Us</a></li>
       <li><a href="https://chat.projectmeshnet.org/#seattlemeshnet">Chat</a></li>
       <li><a href="/cjdns">Software</a></li>
-      <li><a href="http://pics.meshwith.me/">Nodes</a></li>
+      <li><a href="http://pics.meshwith.me/">Node Pics</a></li>
     </ul>
   </div>
 </div>
