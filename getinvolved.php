fee228f7e1802f0dceb3c6fd9539443f279a8d99
thefinn93 <thefinn93@thefinn93.com>
2013-06-21 21:24:36 -0700

    Changed some shit around

--- getinvolved.php
+++ getinvolved.php
@@ -14,7 +14,7 @@
                 <p>
                     We have meetings frequently at <a href="http://www.airlighttimespace.org/location/">ALTSpace</a>. 
                     We have <a href="https://groups.google.com/forum/?fromgroups#!forum/seattlemeshnet">a low traffic mailing list</a>,
-                    an <a href="https://chat.projectmeshnet.org/#seattlemeshnet">IRC channel (chat room)</a> (<code>#seattlemeshnet on irc.efnet.org)</code>,
+                    an <a href="https://chat.projectmeshnet.org/#seattlemeshnet">IRC channel (chat room)</a> (<code>#seattlemeshnet</code> on <code>irc.efnet.org</code>),
                     a <a href="http://www.reddit.com/r/seattlemeshnet">subreddit</a>,
                     a <a href="https://www.facebook.com/pages/Seattle-Meshnet/391420664237243">Facebook</a>, and a <a href="https://twitter.com/seattlemeshnet">twitter</a>.
                 </p>
