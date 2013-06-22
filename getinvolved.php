ed49584a956b5162046b099c5e18bb76ed5aa777
thefinn93 <thefinn93@thefinn93.com>
2013-06-21 21:23:08 -0700

    Added Get Involved page

---
+++ getinvolved.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Seattle Meshnet Project</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+    </head>
+    <body>
+        <? require 'header.php'; ?>
+        <div class="container-fluid">
+            <div class="offset1 span10">
+                <p class="lead">Get involved!</p>
+                <p>
+                    We have meetings frequently at <a href="http://www.airlighttimespace.org/location/">ALTSpace</a>. 
+                    We have <a href="https://groups.google.com/forum/?fromgroups#!forum/seattlemeshnet">a low traffic mailing list</a>,
+                    an <a href="https://chat.projectmeshnet.org/#seattlemeshnet">IRC channel (chat room)</a> (<code>#seattlemeshnet on irc.efnet.org)</code>,
+                    a <a href="http://www.reddit.com/r/seattlemeshnet">subreddit</a>,
+                    a <a href="https://www.facebook.com/pages/Seattle-Meshnet/391420664237243">Facebook</a>, and a <a href="https://twitter.com/seattlemeshnet">twitter</a>.
+                </p>
+
+            </div>
+        </div>
+        <? require 'footer.php'; ?>
+    </body>
+</html>
