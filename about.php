4fb070f376709ec6f915c3448f7884839bc97720
thefinn93 <thefinn93@thefinn93.com>
2013-06-22 15:21:30 -0700

    Made the Get Involved page suck less

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
                 <p>
                     The Seattle Meshnet Project is an attempt to build the physical infrastructure to run cjdns, a decentralized, encrypted, low latency routing protocol. Building off of previous wireless projects, we intend to create a separate network that will be resistant towards attempts to censor or otherwise impede free & legal speech, while also being resistant to natural disasters and other events that might take down ISPs today. 
                 </p>
-
+                <p><a href="/getinvolved" class="btn btn-primary">Get Involved <i class="icon-chevron-sign-right"></i></a></p>
             </div>
         </div>
         <? require 'footer.php'; ?>
--- getinvolved.php
+++ getinvolved.php
@@ -12,12 +12,27 @@
             <div class="offset1 span10">
                 <p class="lead">Get involved!</p>
                 <p>
-                    We have meetings frequently at <a href="http://www.airlighttimespace.org/location/">ALTSpace</a>. 
-                    We have <a href="https://groups.google.com/forum/?fromgroups#!forum/seattlemeshnet">a low traffic mailing list</a>,
-                    an <a href="https://chat.projectmeshnet.org/#seattlemeshnet">IRC channel (chat room)</a> (<code>#seattlemeshnet</code> on <code>irc.efnet.org</code>),
+                    We're trying to decentralize the internet. Come join us.<br />
+                    
+                    The most valuable thing you likely have is your house, aparement,
+                    whatever. Somewhere that you can stick a node. A node is just 
+                    a peice of wireless equipment that can communicate with other nodes.
+                    Generally, the higher up the better, so if we can stick a pole on your
+                    roof, awesome. We have meetings frequently at
+                    <a href="http://www.airlighttimespace.org/location/">ALTSpace</a>. To find out
+                    when the next one is, come over to our <a href="https://chat.projectmeshnet.org/#seattlemeshnet">IRC channel (chat room)</a>
+                    and ask (or check the topic). It's also a great place to ask
+                    any other questions you may have. In additon to that we have
+                    <a href="https://groups.google.com/forum/?fromgroups#!forum/seattlemeshnet">a low traffic mailing list</a>,
                     a <a href="http://www.reddit.com/r/seattlemeshnet">subreddit</a>,
-                    a <a href="https://www.facebook.com/pages/Seattle-Meshnet/391420664237243">Facebook</a>, and a <a href="https://twitter.com/seattlemeshnet">twitter</a>.
+                    and a <a href="https://twitter.com/seattlemeshnet">twitter</a>. All are great places
+                    to get started. If you want to be more involved than just
+                    hosting a node, come by and tell us what you're good at.
+                    Doesn't have to be computery, we need all sorts of people.
                 </p>
+                
+                <p>A final note, as requested by <pre>jasons</pre>, a good way to support us is to 
+                <a href="https://fund.meshwith.me">donate munnies</a>.</p>
 
             </div>
         </div>
