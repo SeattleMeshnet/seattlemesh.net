b8b28783af2ee277179ae5b447f205dfba91aee1
Finn <thefinn93@thefinn93.com>
2014-05-25 03:05:11 -0700

    Removed dead link (node pics)

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
               <li><a href="/about">About Us</a></li>
               <li><a href="http://chat.efnet.org:9090/?channels=%23seattlemeshnet">Chat</a></li>
               <li><a href="/cjdns">Software</a></li>
-              <li><a href="http://pics.meshwith.me/">Node Pics</a></li>
               <li><a href="https://lists.projectmesh.net/cgi-bin/mailman/listinfo/seattle">Mailing List</a></li>
             </ul>
           </div>
